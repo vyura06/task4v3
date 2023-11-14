f5e7dac49672d775fb062623138b208dbf242034
vyura06 <vechorko.yura@gmail.com>
2023-11-14 17:10:53 +0300

    parser

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express()
 const cors = require('cors');
 const pool = require('./database');
 const userRouter = require('./routes/user.routes')
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 8080
 
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
 app.use('/api', userRouter)
-app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
